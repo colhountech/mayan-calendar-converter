be7687fa60a82536911336a5797c74f369e89e02
Micheal Colhoun <micheal@colhountech.com>
2023-05-26 20:45:58 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class MayanCalendarConverter
+{
+    public static void Main()
+    {
+        // Example usage:
+        DateTime gregorianDate = new DateTime(2023, 4, 28);
+        string tzolkinDate = ConvertToTzolkin(gregorianDate);
+        Console.WriteLine("Mayan Tzolk'in date: " + tzolkinDate);
+    }
+
+    public static string ConvertToTzolkin(DateTime gregorianDate)
+    {
+        DateTime startDate = new DateTime(2023, 04, 28); // Gregorian date corresponding to Tzolk'in 1 Imix
+        
+        int totalDays = (gregorianDate - startDate).Days;
+        int tzolkinNumber = (totalDays % 13) + 1;
+        int tzolkinIndex = (totalDays % 20);
+
+        string[] daySigns = { "Imix", "Ik'", "Ak'b'al", "K'an", "Chikchan", "Kimi", "Manik'", "Lamat", "Muluk", "Ok", "Chuwen", "Eb'", "B'en", "Ix", "Men", "K'ib'", "Kab'an", "Etz'nab'", "Kawak", "Ajaw" };
+
+        string tzolkinDate = tzolkinNumber + " " + daySigns[tzolkinIndex];
+        return tzolkinDate;
+    }
+}
